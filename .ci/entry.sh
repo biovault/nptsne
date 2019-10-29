266ced1f2afcd9e1359a3d8e627005fcfc3ca3b9
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-29 11:04:22 +0100

    Use anaconda to get correct python version in conan docker  for linux
    Added to the entry script (entry.sh) that is run at docker startup

--- .ci/entry.sh
+++ .ci/entry.sh
@@ -1,3 +1,17 @@
-if [ "$(uname -s)" == 'Darwin' ] || [ "$(uname -s)" == 'Linux' ]; then
-    pip install scikit-build
+#!/usr/bin/env bash
+
+if [ "$(uname -s)" == 'Linux' ]; then
+    wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+    bash miniconda.sh -b -p $HOME/miniconda
+    export PATH="$HOME/miniconda/bin:$PATH"
+    conda config --set always_yes yes --set changeps1 no
+    conda config --add channels conda-forge
+    conda update -q conda
+    conda install -q conda-build
+    conda create -q -n build_env python=$LINUX_PYTHON
+    source activate build_env  
+    conda install -n build_env cmake
+    conda install -c conda-forge conan 
+    conda install -c conda-forge scikit-build
+    pip install conan_package_tools bincrafters_package_tools
 fi 
--- build.py
+++ build.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     if platform.system() == "Windows":
         docker_entry_script = None
     else:
-        docker_entry_script = "pip install scikit-build"    
+        docker_entry_script = "./.ci/entry.sh"    
     builder = build_template_default.get_builder(
         reference="nptsne/1.0.0rc1@lkeb/stable",  # suppress conan using the feature/aaa
         docker_entry_script=docker_entry_script
