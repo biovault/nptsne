1d49bc964b295b2b09ed795e0c5a7c2ae60454ca
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-29 15:28:45 +0100

    sudo the miniconda install

--- .ci/entry.sh
+++ .ci/entry.sh
@@ -1,5 +1,5 @@
 wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p $HOME/miniconda
+sudo bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
