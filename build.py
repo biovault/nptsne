250a25b2d1dc4d3d55dec54c3c9bc1cf2c2d500c
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-29 15:32:49 +0100

    sudo the miniconda install

--- build.py
+++ build.py
@@ -19,7 +19,7 @@ if __name__ == "__main__":
     docker_entry_script = None
     if platform.system() == "Linux":
         docker_entry_script = """wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash miniconda.sh -b -p $HOME/miniconda
+sudo bash miniconda.sh -b -p $HOME/miniconda
 export PATH="$HOME/miniconda/bin:$PATH"
 conda config --set always_yes yes --set changeps1 no
 conda config --add channels conda-forge
