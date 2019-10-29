f46f31f6a88bfeafc03b0bee1dab537fd814e829
Baldur van Lew <baldurvanlew@gmail.com>
2019-10-29 14:13:20 +0100

    Simplify entry script - is eval-ed

--- .ci/entry.sh
+++ .ci/entry.sh
@@ -1,19 +1,16 @@
-#!/usr/bin/env bash
-
-if [ "$(uname -s)" == 'Linux' ]; then
-    wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-    bash miniconda.sh -b -p $HOME/miniconda
-    export PATH="$HOME/miniconda/bin:$PATH"
-    conda config --set always_yes yes --set changeps1 no
-    conda config --add channels conda-forge
-    conda update -q conda
-    conda install -q conda-build
-    echo Creating conda env for python version $CONAN_LINUX_PYTHON
-    conda create -q -n build_env python=$CONAN_LINUX_PYTHON
-    source activate build_env  
-    conda install -n build_env cmake
-    conda install -c conda-forge conan 
-    conda install -c conda-forge scikit-build
-    pip install conan_package_tools bincrafters_package_tools
-    echo Conda python version `python --version`
-fi 
+wget -O miniconda.sh https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+bash miniconda.sh -b -p $HOME/miniconda
+export PATH="$HOME/miniconda/bin:$PATH"
+conda config --set always_yes yes --set changeps1 no
+conda config --add channels conda-forge
+conda update -q conda
+conda install -q conda-build
+echo Creating conda env for python version $CONAN_LINUX_PYTHON
+conda create -q -n build_env python=$CONAN_LINUX_PYTHON
+source activate build_env  
+conda install -n build_env cmake
+conda install -c conda-forge conan 
+conda install -c conda-forge scikit-build
+pip install conan_package_tools bincrafters_package_tools
+echo Conda python version `python --version`
+ 
