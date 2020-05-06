d924723b49b685c1a62ed0c5ad4d20afa11f39b1
Baldur van Lew <baldurvanlew@gmail.com>
2020-05-06 17:30:20 +0200

    [skip travis] Try the md5 upload

--- cmake/UploadWheel.cmake
+++ cmake/UploadWheel.cmake
@@ -4,5 +4,9 @@ set(CONAN_PASSWORD $ENV{CONAN_PASSWORD})
 message(STATUS "Looking for: ${WHEEL_GLOB}")
 file(GLOB NPTSNE_WHEEL "${WHEEL_GLOB}")
 message(STATUS "About to upload: ${NPTSNE_WHEEL}")
+file(MD5 ${NPTSNE_WHEEL} NPTSNE_WHEEL_MD5)
+message(STATUS "With md5: ${NPTSNE_WHEEL_MD5}")
 get_filename_component(NPTSNE_WHEEL_NAME ${NPTSNE_WHEEL} NAME)
-file(UPLOAD ${NPTSNE_WHEEL} "http://cytosplore.lumc.nl:8081/artifactory/wheels/nptsne/latest/${NPTSNE_WHEEL_NAME}" USERPWD ${CONAN_LOGIN_USERNAME}:${CONAN_PASSWORD})
+file(UPLOAD ${NPTSNE_WHEEL} "http://cytosplore.lumc.nl:8081/artifactory/wheels/nptsne/latest/${NPTSNE_WHEEL_NAME}"
+    USERPWD ${CONAN_LOGIN_USERNAME}:${CONAN_PASSWORD}
+    HTTPHEADER "X-Checksum-md5: ${NPTSNE_WHEEL_MD5})
