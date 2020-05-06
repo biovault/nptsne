0fcdbc7049ea87b9723e24ba6c78cfb6c6ce465b
Baldur van Lew <baldurvanlew@gmail.com>
2020-05-06 16:41:15 +0200

    debug the wheel glob

--- cmake/UploadWheel.cmake
+++ cmake/UploadWheel.cmake
@@ -1,6 +1,7 @@
 set(CONAN_LOGIN_USERNAME $ENV{CONAN_LOGIN_USERNAME})
 set(CONAN_PASSWORD $ENV{CONAN_PASSWORD})
 
+message(STATUS "Looking for: ${WHEEL_GLOB}")
 find_file(NPTSNE_WHEEL "${WHEEL_GLOB}")
 message(STATUS "About to upload: ${NPTSNE_WHEEL}")
 file(UPLOAD ${NPTSNE_WHEEL} "http://cytosplore.lumc.nl:8081/artifactory/wheels/nptsne/latest" USERPWD ${CONAN_LOGIN_USERNAME}:${CONAN_PASSWORD})
