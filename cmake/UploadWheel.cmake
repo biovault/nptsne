35ab7aa3cdc34b0ec3470e51d57730c5df4dbda2
Baldur van Lew <baldurvanlew@gmail.com>
2020-05-06 16:16:47 +0200

    Add upload via helper script

---
+++ cmake/UploadWheel.cmake
@@ -0,0 +1,6 @@
+set(CONAN_LOGIN_USERNAME ENV(CONAN_LOGIN_USERNAME))
+set(CONAN_PASSWORD ENV(CONAN_PASWORD))
+
+find_file(NPTSNE_WHEEL "${WHEEL_GLOB}")
+message(STATUS "About to upload: ${NPTSNE_WHEEL}")
+file(UPLOAD ${NPTSNE_WHEEL} "http://cytosplore.lumc.nl:8081/artifactory/wheels/nptsne/latest" USERPWD ${CONAN_LOGIN_USERNAME}:${CONAN_PASSWORD})
