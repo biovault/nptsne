7d6672eff28c74f41a2647c4a7fdc99ba4c05af4
B. van Lew <b.van_lew@lumc.nl>
2020-06-13 19:39:18 +0200

    Explicit paths

--- docsource/sphinx_rst_build.bat
+++ docsource/sphinx_rst_build.bat
@@ -1,8 +1,8 @@
 REM sphinx-build [options] source output
-rd /s /q .\source\generated
-rd /s /q .\build\generated
-rd /s /q .\nptsnedoc\source\generated
-sphinx-build -b rst .\source .\build
-xcopy /Y /S .\build\generated .\nptsnedoc\source\generated\
-copy /Y .\build\nptsne_module.rst .\nptsnedoc\source\
-copy /Y ..\version.txt .\nptsnedoc\
+rd /s /q %~dp0\source\generated
+rd /s /q %~dp0\build\generated
+rd /s /q %~dp0\nptsnedoc\source\generated
+sphinx-build -b rst %~dp0\source %~dp0\build
+xcopy /Y /S %~dp0\build\generated %~dp0\nptsnedoc\source\generated\
+copy /Y %~dp0\build\nptsne_module.rst %~dp0\nptsnedoc\source\
+copy /Y %~dp0\..\version.txt %~dp0\nptsnedoc\
