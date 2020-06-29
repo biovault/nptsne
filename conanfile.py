f10961b7f521931b6bdd6bd1f1245512e7a902b0
B. van Lew <b.van_lew@lumc.nl>
2020-06-29 10:55:49 +0200

    Brew install in CMakeLists

--- conanfile.py
+++ conanfile.py
@@ -53,9 +53,7 @@ class NptsneConan(ConanFile):
                 installer.install('libglfw3')
                 installer.install('libglfw3-dev')
         if tools.os_info.is_macos:           
-            installer = tools.SystemPackageTool() 
-            installer.install('llvm')
-            installer.install('libomp')            
+            installer = tools.SystemPackageTool()           
             installer.install('lz4')
             installer.install('glfw')
             
