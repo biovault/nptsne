84d7464cc4de3076e47b73225aa9c824e3cb8edb
B. van Lew <b.van_lew@lumc.nl>
2020-06-30 20:22:15 +0200

    Cleanup CMake and doctest

--- test/run_doctest.py
+++ test/run_doctest.py
@@ -1,37 +1,41 @@
+"""Doctests for nptsne"""
+import doctest
+from doctest import REPORT_NDIFF, ELLIPSIS
 import nptsne
+import nptsne.libs._nptsne
 import numpy as np
 
-def make_test_globals():
 
+def make_test_globals():
+    """Prepare global objects used in the doctests"""
     print("Prepare doctest globals")
     # Test using a small data sample of 10000 random integers 0-255
-    # with 16 dimensions    
-    data = np.random.randint(256, size=(10000,16))
+    # with 16 dimensions
+    data = np.random.randint(256, size=(10000, 16))
     # Create a sample hsne with 3 levels and
-    # save this to a sample file    
+    # save this to a sample file
     hsne = nptsne.HSne(True)
     hsne.create_hsne(data, 3)
     file_name = "rnd10000x16.hsne"
     hsne.save(file_name)
-    
+
     print("End prepare doctest globals")
     return {
         "sample_hsne": hsne,
-        "sample_scale0": hsne.get_scale(0),      
-        "sample_scale1": hsne.get_scale(1), 
-        "sample_scale2": hsne.get_scale(2),         
+        "sample_scale0": hsne.get_scale(0),
+        "sample_scale1": hsne.get_scale(1),
+        "sample_scale2": hsne.get_scale(2),
         "sample_hsne_file": file_name,
         "sample_data": data
     }
 
+
 if __name__ == "__main__":
-    import doctest
-    from doctest import REPORT_NDIFF,ELLIPSIS
+    print("Starting doctest")
     doctest.testmod(nptsne, verbose=True)
     # Doctest will not find the extension library
-    import nptsne.libs._nptsne
     print("Starting doctest")
-    doctest.testmod(nptsne.libs._nptsne, 
-        verbose=True, 
-        optionflags=REPORT_NDIFF|ELLIPSIS, 
-        globs = make_test_globals())
+    doctest.testmod(nptsne.libs._nptsne,
+                    verbose=True,
+                    optionflags=REPORT_NDIFF | ELLIPSIS,
+                    globs=make_test_globals())
