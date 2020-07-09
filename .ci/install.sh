cbe05475d5fe5cfd4175960c7f958062db72ac32
B. van Lew <b.van_lew@lumc.nl>
2020-07-09 18:00:32 +0200

    Force pip3 on linux and travis

--- .ci/install.sh
+++ .ci/install.sh
@@ -18,12 +18,12 @@ set -ex
     # pip install six==1.12.0
     # pip install conan==1.24.1      
 # fi
-pip install six==1.12.0
-pip install conan==1.24.1 
-pip install scikit-build    
-pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
-pip install cmake
-pip install cibuildwheel==1.5.2
+pip3 install six==1.12.0
+pip3 install conan==1.24.1 
+pip3 install scikit-build    
+pip3 install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
+pip3 install cmake
+pip3 install cibuildwheel==1.5.2
 echo Python version:
 python --version 
 
