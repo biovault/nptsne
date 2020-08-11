6f3a326ab140f18cb0bc3f23592862719eb79239
B. van Lew <b.van_lew@lumc.nl>
2020-08-11 10:57:10 +0200

    Move build preparation to text file and source it
    This is more reliable and easier to trace

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,12 +1,6 @@
-#!/usr/bin/env bash
-
-set -ex
-
-pip3 install six==1.12.0
-pip3 install conan==1.24.1 
-pip3 install scikit-build    
-pip3 install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
-pip3 install cmake
+pip install six==1.12.0
+pip install conan==1.24.1 
+pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
 echo Python version:
 python --version 
 
