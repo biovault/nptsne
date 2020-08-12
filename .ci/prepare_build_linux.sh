b622329f6acd17e4dc1cae836bef22716806b220
B. van Lew <b.van_lew@lumc.nl>
2020-08-12 15:03:11 +0200

    Move cmake and conan install to foreground
    Correctly sets paths

--- .ci/prepare_build_linux.sh
+++ .ci/prepare_build_linux.sh
@@ -2,15 +2,6 @@ echo ** Start build environment preparation
 yum remove -y cmake
 yum install -y lz4-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
 pip install -Iv six==1.12.0
-pip install conan==1.24.1 
 pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
-echo Python version:
-python --version 
-pip install cmake
-echo CMake version
-cmake --version
-echo Prepare conan
-conan user
-conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
 echo ** End build environment preparation
 
---
+++ .ci/prepare_conan.sh
@@ -0,0 +1,7 @@
+echo Python version:
+python --version 
+echo CMake version
+cmake --version
+echo Prepare conan
+conan user
+conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
