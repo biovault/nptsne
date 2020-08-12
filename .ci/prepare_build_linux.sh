488c02f6a3346a3dae38b2aa67ba511f817cc09f
B. van Lew <b.van_lew@lumc.nl>
2020-08-12 16:32:22 +0200

    Debug setup

--- .ci/prepare_build_linux.sh
+++ .ci/prepare_build_linux.sh
@@ -3,16 +3,15 @@ echo ** Start build environment preparation
 yum remove -y cmake
 yum install -y lz4-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
 
-PY38_BIN=/opt/python/cp38-cp38m/bin
-$PY38_BIN/pip install cmake
-ln -s $PY38_BIN/cmake /usr/local/bin/cmake
+pip install cmake
+ln -s `which cmake` /usr/local/bin/cmake
 echo "CMake version:"
 cmake --version
 
-$PY38_BIN/pip install -Iv six==1.12.0
-$PY38_BIN/pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
-$PY38_BIN/pip install conan==1.24.1
-conan=`which conan`; ln -s $PY38_BIN/conan $conan
+pip install -Iv six==1.12.0
+pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
+pip install conan==1.24.1
+ln -s `which conan` /usr/local/bin/conan
 echo "conan version:"
 conan --version
 echo ** End build environment preparation
