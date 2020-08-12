54f671bc0ac89d19748c99a66b91415aeaa37395
B. van Lew <b.van_lew@lumc.nl>
2020-08-12 16:06:04 +0200

    Fix conan link

--- .ci/prepare_build_linux.sh
+++ .ci/prepare_build_linux.sh
@@ -8,9 +8,9 @@ ln -s $PY37_BIN/cmake /usr/local/bin/cmake
 echo "CMake version:"
 cmake --version
 
-pip install -Iv six==1.12.0
-pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
-pip install conan==1.24.1
+$PY37_BIN/pip install -Iv six==1.12.0
+$PY37_BIN/pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
+$PY37_BIN/pip install conan==1.24.1
 conan=`which conan`; ln -s $PY37_BIN/conan $conan
 echo "conan version:"
 conan --version
