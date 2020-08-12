7508c17c49162334c2ef62234e64b92c72d90549
B. van Lew <b.van_lew@lumc.nl>
2020-08-12 13:56:47 +0200

    Force the conan compatible version of six

--- .ci/prepare_build_linux.sh
+++ .ci/prepare_build_linux.sh
@@ -1,7 +1,7 @@
 echo ** Start build environment preparation
 yum remove -y cmake
 yum install -y lz4-devel libXrandr-devel libXinerama-devel libXcursor-devel libXi-devel
-pip install six==1.12.0
+pip install -Iv six==1.12.0
 pip install conan==1.24.1 
 pip install conan_package_tools==0.32.2 bincrafters_package_tools==0.26.3
 echo Python version:
