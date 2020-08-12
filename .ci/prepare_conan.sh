bc49b9bb74f92a08626cf19df933c16f0382b76c
B. van Lew <b.van_lew@lumc.nl>
2020-08-12 17:38:41 +0200

    verify the conan version before all builds

--- .ci/prepare_conan.sh
+++ .ci/prepare_conan.sh
@@ -2,6 +2,8 @@ echo Python version:
 python --version 
 echo CMake version
 cmake --version
+echo conan version
+conan --version
 echo Prepare conan
 conan user
 conan remote add bincrafters https://api.bintray.com/conan/bincrafters/public-conan
