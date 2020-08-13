f5b2870e5200ab29434212c0e76855da5c398114
B. van Lew <b.van_lew@lumc.nl>
2020-08-13 09:29:55 +0200

    Upload script for a list of wheels

--- cmake/UploadWheel.cmake
+++ cmake/UploadWheel.cmake
@@ -3,13 +3,14 @@ set(CONAN_PASSWORD $ENV{CONAN_PASSWORD})
 
 message(STATUS "Looking for: ${WHEEL_GLOB}")
 file(GLOB NPTSNE_WHEEL "${WHEEL_GLOB}")
-message(STATUS "About to upload: ${NPTSNE_WHEEL}")
+message(STATUS "Found files: ${NPTSNE_WHEEL}")
 
-foreach(WHEEL in LISTS NPTSNE_WHEEL)
+foreach(WHEEL IN LISTS NPTSNE_WHEEL)
+    message(STATUS "Upload file: ${WHEEL}")
     file(MD5 ${WHEEL} NPTSNE_WHEEL_MD5)
-    message(STATUS "With md5: ${NPTSNE_WHEEL_MD5}")
+    message(STATUS "${WHEEL} with md5: ${NPTSNE_WHEEL_MD5}")
     get_filename_component(NPTSNE_WHEEL_NAME ${WHEEL} NAME)
     file(UPLOAD ${WHEEL} "http://cytosplore.lumc.nl:8081/artifactory/wheels/nptsne/latest/${NPTSNE_WHEEL_NAME}"
         USERPWD ${CONAN_LOGIN_USERNAME}:${CONAN_PASSWORD}
         HTTPHEADER "X-Checksum-md5: ${NPTSNE_WHEEL_MD5}")
-endforreach()
+endforeach()
