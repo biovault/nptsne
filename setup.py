74cde30bbee3cf0860330c3712814d4b16bf099e
B. van Lew <b.van_lew@lumc.nl>
2020-08-18 12:48:48 +0200

    Derive a cross CI platform build number from git commit count

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ import tempfile
 import os
 
 templibdir = os.environ.get('LIBSDIR', '/tmp/cibwlibsdir')
-print("GIT_DERIVED_BUILD_NUMBER = {}".format(os.environ.get('GIT_DERIVED_BUILD_NUMBER', 'not found')) 
+print("GIT_DERIVED_BUILD_NUMBER = {}".format(os.environ.get('GIT_DERIVED_BUILD_NUMBER', 'not found')))
+ 
 setup(
     ext_modules=[CMakeExtension('_nptsne', 'nptsne', templibdir=templibdir)],  # provide the extension name and package_name
     cmdclass=dict(build_ext=CMakeBuild),
