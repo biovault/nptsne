9f49081bf0d85a70a2adac1e714de9d77c466c07
B. van Lew <b.van_lew@lumc.nl>
2020-08-18 14:19:48 +0200

    Integrate the build number derivation in the setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,15 @@ from cmake_utils import CMakeExtension, CMakeBuild
 import tempfile
 import os
 
+def get_git_derived_build_number():
+    from git import Repo
+    repo = Repo('.')
+    branch=repo.active_branch.name
+    commits = list(repo.iter_commits(branch, paths='./src/nptsne/_version.txt', max_count=1))
+    return len(list(repo.iter_commits(rev='{}^..{}'.format(commits[0].hexsha, branch))))
+
 templibdir = os.environ.get('LIBSDIR', '/tmp/cibwlibsdir')
-print("GIT_DERIVED_BUILD_NUMBER = {}".format(os.environ.get('GIT_DERIVED_BUILD_NUMBER', 'not found')))
+print("GIT_DERIVED_BUILD_NUMBER = {}".format(get_git_derived_build_number()))
  
 setup(
     ext_modules=[CMakeExtension('_nptsne', 'nptsne', templibdir=templibdir)],  # provide the extension name and package_name
