1465886891a9e0a3bcb9d1e63dec3bb6e391cd50
B. van Lew <b.van_lew@lumc.nl>
2020-08-18 14:47:23 +0200

    Where possible derive branch from CI data

--- setup.py
+++ setup.py
@@ -3,10 +3,21 @@ from cmake_utils import CMakeExtension, CMakeBuild
 import tempfile
 import os
 
+
 def get_git_derived_build_number():
     from git import Repo
     repo = Repo('.')
-    branch=repo.active_branch.name
+    is_ci = os.environ.get('CI', 'false').lower() == 'true'
+    is_travis = os.environ.get('TRAVIS', 'false').lower() == 'true'
+    is_appveyor = os.environ.get('APPVEYOR', 'false').lower() == 'true'
+    if not is_ci:
+        branch=repo.active_branch.name
+    else
+        if is_appveyor:
+            branch = os.environ.get('APPVEYOR_REPO_BRANCH')
+        if is_travis
+            branch = os.environ.get('TRAVIS_BRANCH')
+
     commits = list(repo.iter_commits(branch, paths='./src/nptsne/_version.txt', max_count=1))
     return len(list(repo.iter_commits(rev='{}^..{}'.format(commits[0].hexsha, branch))))
 
