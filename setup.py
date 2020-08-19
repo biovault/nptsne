677ec73208a2fa76467e44db38a5a170f3d080eb
B. van Lew <b.van_lew@lumc.nl>
2020-08-19 14:46:58 +0200

    Generate version suffix based on branch and tag
    has tag = release.* then is <version>
    has branch = release.* then is <version>rc<build_number>
    otherwise = <version>dev<build_number>

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from cmake_utils import CMakeExtension, CMakeBuild
 import tempfile
 import os
 
+def get_current_tag(repo):
+    return next((tag for tag in repo.tags if tag.commit == repo.head.commit), None)
 
-def get_git_derived_build_number():
-    from git import Repo
-    repo = Repo('.')
-    is_ci = os.environ.get('CI', 'false').lower() == 'true'
-    is_travis = os.environ.get('TRAVIS', 'false').lower() == 'true'
-    is_appveyor = os.environ.get('APPVEYOR', 'false').lower() == 'true'
+def get_repo_branch(repo):
+    branch = ''
     if not is_ci:
         branch=repo.active_branch.name
     else:
@@ -17,19 +15,33 @@ def get_git_derived_build_number():
             branch = os.environ.get('APPVEYOR_REPO_BRANCH')
         if is_travis:
             branch = os.environ.get('TRAVIS_BRANCH')
+    return branch
+
+def get_git_derived_build_number(repo, branch):
     print('Derive number for branch: ', branch)
     commits = list(repo.iter_commits(branch, paths='./src/nptsne/_version.txt', max_count=1))
     print('Derive number for commit (of _version.txt): ', commits[0].hexsha)
     return len(list(repo.iter_commits(rev='{}^..{}'.format(commits[0].hexsha, branch))))
 
 def get_version():
+    from git import Repo
+    repo = Repo('.')
+    tag = get_current_tag(repo)
     with open('./src/nptsne/_version.txt') as f:
-        raw_version = f.read()
-    return raw_version + '+' + str(get_git_derived_build_number())
+        raw_version = f.read().strip()
     
+    if tag.startswith('release')
+        return raw_version
     
+    build_number = get_git_derived_build_number(repo, branch)
+
+    if branch.startswith('release')
+        return raw_version + 'rc' + build_number
+        
+    return raw_version + 'dev'+ build_number
+
 templibdir = os.environ.get('LIBSDIR', '/tmp/cibwlibsdir')
-print("GIT_DERIVED_BUILD_NUMBER = {}".format(get_git_derived_build_number()))
+print("git derived version = {}".format(get_version()))
  
 setup(
     # Always append the build number for tracking purposes - this fits with PEP427
