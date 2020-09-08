07ef0b21fb29f3b53a44e04e4af729aa2368e8f3
Baldur van Lew <baldurvanlew@gmail.com>
2020-09-08 14:33:38 +0200

    Fix cilibsdir root

--- cmake_utils.py
+++ cmake_utils.py
@@ -74,6 +74,7 @@ class CMakeBuild(build_ext):
         # Also limit Windows to single config build -
         # Causes Conan to load a single set of libs
         cmake_args += ['-DCMAKE_BUILD_TYPE=' + cfg, '-DTEMP_LIBS_DIR=' + ext.templibdir]
+        lib_install_args += ['-DTEMP_LIBS_DIR=' + ext.templibdir]
         if platform.system() == "Windows":
             #VS can produce separate RELEASE or DEBUG outputs
             cmake_args += ['-DCMAKE_LIBRARY_OUTPUT_DIRECTORY_{}={}'.format(cfg.upper(), liboutputdir)]
@@ -109,7 +110,7 @@ class CMakeBuild(build_ext):
         # os.environ['LD_LIBRARY_PATH'] = liboutputdir
         
         print('Move the conan dependencies for wheel fix-up')
-        subprocess.check_call(['cmake', '--build', '.', '--target',  'bundle_libs', '--config', cfg], cwd=self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.', '--target',  'bundle_libs', '--config', cfg] + lib_install_args, cwd=self.build_temp)
                               
         print("Files in output dir: ", os.listdir(liboutputdir)) 
         print("Files in temp libs dir: ", os.listdir(ext.templibdir)) 
