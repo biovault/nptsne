fa8ffa97603d9d29fab0b6fdc961f13db22abc43
B. van Lew <b.van_lew@lumc.nl>
2021-02-10 17:37:35 +0100

    Move certfile fix  to prepare_conan.sh

--- .ci/prepare_conan.sh
+++ .ci/prepare_conan.sh
@@ -6,4 +6,5 @@ echo conan version
 conan --version
 echo Prepare conan
 conan user
+cat cert.pem >> ~/.conan/cacert.pem
 conan remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan
