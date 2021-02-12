4af2e6126f58452384c892eb3fbd689bfde3c3ed
B. van Lew <b.van_lew@lumc.nl>
2021-02-12 16:59:52 +0100

    MOved windows setup to batch file

---
+++ .ci/setup.bat
@@ -0,0 +1,14 @@
+rd /s /q %TMP%\\cibwlibsdir
+mkdir %TMP%\\cibwlibsdir 
+pip install --upgrade pip
+pip install -r requirements.txt
+type %1\\cert.pem >> %USERPROFILE%\\.conan\\cacert.pem 
+echo Python version:
+python --version
+echo CMake version
+cmake --version 
+echo conan version
+conan --version 
+echo Prepare conan
+conan user 
+conan remote add -f bincrafters https://api.bintray.com/conan/bincrafters/public-conan
