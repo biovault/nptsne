bcd392e2472352527250ef012cb314240a751c64
B. van Lew <b.van_lew@lumc.nl>
2021-02-15 17:29:24 +0100

    Force no brew update from conan

--- conanfile.py
+++ conanfile.py
@@ -45,6 +45,7 @@ class NptsneConan(ConanFile):
                 installer.install('liblz4-dev')
             # Centos like: -See prepare_build_linux.sh
         if tools.os_info.is_macos:
+            os.environ['HOMEBREW_NO_AUTO_UPDATE'] = '1'
             installer = tools.SystemPackageTool()
             installer.install('libomp')
             installer.install('lz4')
