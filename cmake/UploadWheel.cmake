e9cc7d350a2e9df284a1be53a4a1717376c0b618
B. van Lew <b.van_lew@lumc.nl>
2021-02-16 11:08:39 +0100

    Use the cert.pem for wget and cmake up/down-load

--- cmake/UploadWheel.cmake
+++ cmake/UploadWheel.cmake
@@ -1,6 +1,7 @@
 # Provide the following defines
 # WHEEL_GLOB - a glob wildcard expression that will resolve to a list of wheel files
 # BUILD_NUMBER - a unique number for this build used to store the wheels
+# CERT_FILE - the certificate file for the artifactory (needed due to CA issues)
 set(CONAN_LOGIN_USERNAME $ENV{CONAN_LOGIN_USERNAME})
 set(CONAN_PASSWORD $ENV{CONAN_PASSWORD})
 
@@ -13,7 +14,8 @@ foreach(WHEEL IN LISTS NPTSNE_WHEEL)
     file(MD5 ${WHEEL} NPTSNE_WHEEL_MD5)
     message(STATUS "${WHEEL} with md5: ${NPTSNE_WHEEL_MD5}")
     get_filename_component(NPTSNE_WHEEL_NAME ${WHEEL} NAME)
-    file(UPLOAD ${WHEEL} "http://cytosplore.lumc.nl:8081/artifactory/wheels/nptsne/build_${BUILD_NUMBER}/${NPTSNE_WHEEL_NAME}"
+    file(UPLOAD ${WHEEL} "https://lkeb-artifactory.lumc.nl/artifactory/wheels/nptsne/build_${BUILD_NUMBER}/${NPTSNE_WHEEL_NAME}"
         USERPWD ${CONAN_LOGIN_USERNAME}:${CONAN_PASSWORD}
-        HTTPHEADER "X-Checksum-md5: ${NPTSNE_WHEEL_MD5}")
+        HTTPHEADER "X-Checksum-md5: ${NPTSNE_WHEEL_MD5}"
+        TLS_CAINFO ${CERT_FILE})
 endforeach()
