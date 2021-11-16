5aecf4a18456680f5562f33e34bf6edf04e9c1be
B. van Lew <b.van_lew@lumc.nl>
2021-11-16 09:51:39 +0100

    Test readthedocs

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 from pathlib import Path
-p = Path('.')
+
+p = Path(".")
 sys.path.append(str(p.resolve()))
 
 from setuptools import setup
@@ -9,12 +10,12 @@ from tools.version_util import get_version
 from pathlib import Path
 import tempfile
 
-print("starting setup")
+# print("starting setup")
 #  This temporary directory is used to collect libs
 #  for inclusion in the wheel
 templibdir = Path(Path(tempfile.gettempdir()), "cibwlibsdir")
 # print('cibwlibsdir at: ', templibdir)
-print("pre setup call")
+# print("pre setup call")
 setup(
     # Always append the build number for tracking purposes - this fits with PEP427
     version=get_version(Path(__file__).resolve().parent),
