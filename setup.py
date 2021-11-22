29221000dcc1868e1e65921cbe2c1c7d91843fc7
bldrvnlw <b.van_lew@lumc.nl>
2021-11-22 18:12:00 +0100

    Update version during build with alpha beta suffix

--- setup.py
+++ setup.py
@@ -16,9 +16,14 @@ import tempfile
 templibdir = Path(Path(tempfile.gettempdir()), "cibwlibsdir")
 # print('cibwlibsdir at: ', templibdir)
 # print(f"****Derived version: {get_version(Path(__file__).resolve().parent)}")
+vertxt = get_version(Path(__file__).resolve().parent)
+script_path = Path(__file__).absolute()
+with open(Path(script_path, "nptsne", "_version.txt", "w")) as verfile:
+    verfile.write(vertxt)
+
 setup(
     # Always append the build number for tracking purposes - this fits with PEP427
-    version=get_version(Path(__file__).resolve().parent),
+    version=vertxt,
     ext_modules=[
         CMakeExtension("_nptsne", "nptsne", templibdir=str(templibdir))
     ],  # provide the extension name and package_name
