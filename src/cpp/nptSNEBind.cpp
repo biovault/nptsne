dce6b6804759440ae2c14c76c84a7197f03b5d76
B. van Lew <b.van_lew@lumc.nl>
2021-11-30 15:03:07 +0100

    Fix redef bug

--- src/cpp/nptSNEBind.cpp
+++ src/cpp/nptSNEBind.cpp
@@ -49,7 +49,7 @@ PYBIND11_MODULE(_nptsne, m)
         .value("Hamming", hdi::dr::knn_distance_metric::KNN_METRIC_HAMMING)
         .value("Dot", hdi::dr::knn_distance_metric::KNN_METRIC_DOT);
 
-    py::class_<hdi::dr::knn_library> pyKnn_distance_metric(m, "knn_library");
+    py::class_<hdi::dr::knn_library> pyKnn_library(m, "knn_library");
     py::enum_<hdi::dr::knn_library> enumKA(m, "KnnAlgorithm", py::arithmetic(), R"pbdoc(
             Enumeration used to select the knn algorithm used. Three possibilities are
             supported:
