de6967a381fd4ca8e552642aade96a7cb682102a
Baldur van Lew <b.van_lew@lumc.nl>
2026-04-21 15:03:01 +0200

    [skip ci] Allow detached head building

--- tools/version_util.py
+++ tools/version_util.py
@@ -1,5 +1,7 @@
 import os
 from git import Repo
+from git.exc import InvalidGitRepositoryError
+
 from pathlib import Path
 
 def get_branch_name(repo_path="./"):
@@ -15,5 +17,19 @@ def get_branch_name(repo_path="./"):
     """
     not_rtd = os.environ.get("READTHEDOCS") != "True"
     assert not_rtd  # don't use this on read the docs
-    repo = Repo(repo_path)
-    return repo.active_branch.name
+    repo = None
+    try:
+      repo = Repo(repo_path)
+
+    except InvalidGitRepositoryError:
+        return "0+unknown"
+
+    try:
+        # works only when on a branch
+        return repo.active_branch.name
+    except TypeError:
+        # detached HEAD
+        # If HEAD is detached, do NOT use repo.head.reference / active_branch
+        sha = repo.head.commit.hexsha[:8]
+        return f"0+g{sha}"
+    
